7317ae441dddcce6e13692dbc8f19b1d89c9703a
Dhayanandhan <dhayaec@gmail.com>
2022-03-26 13:42:59 +0530

    test push

--- lib/constants/constants.ts
+++ lib/constants/constants.ts
@@ -21,3 +21,5 @@ export const percentages = {
   target: [0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.3, 1.5, 1.7, 2, 2.3, 2.5, 2.7, 2.9, 3],
   sl: [0.2, 0.3, 0.4, 0.5, 0.8, 1, 1.5, 1.7, 2],
 }
+
+export const test = { name: 'one' }
