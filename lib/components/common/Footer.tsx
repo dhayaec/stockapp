a1297bf7c471d8d70a35942551e8b89545db84ea
Dhayanandhan <dhayaec@gmail.com>
2022-03-27 06:44:27 +0530

    fix footer

--- lib/components/common/Footer.tsx
+++ lib/components/common/Footer.tsx
@@ -1,11 +1,9 @@
 export default function Footer() {
   return (
-    <footer className='fixed bottom-0'>
-      <div className='flex justify-center '>
-        <p className='text-center text-xs text-gray-400'>
-          &copy; {new Date().getFullYear()} Stockr App
-        </p>
-      </div>
+    <footer className='fixed bottom-0 m-auto w-full'>
+      <p className='text-center text-xs text-gray-500'>
+        &copy; {new Date().getFullYear()} Stockr App
+      </p>
     </footer>
   )
 }
