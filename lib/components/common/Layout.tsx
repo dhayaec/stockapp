32503ba6128bda2460e7024b13ad0e021f0ed426
Dhayanandhan <dhayaec@gmail.com>
2022-03-27 07:02:10 +0530

    add lint-staged

--- lib/components/common/Layout.tsx
+++ lib/components/common/Layout.tsx
@@ -1,13 +1,16 @@
+import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 
-export default function Layout({ children }: any) {
+type LayoutProps = {
+  children: React.ReactChildren
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
-    <div className='container mx-auto w-full lg:w-10/12'>
+    <div className='container mx-auto w-full lg:w-8/12'>
       <Header />
-      <div className='flex min-h-screen flex-col justify-start py-2 px-2'>
-        {children}
-      </div>
+      <div className='flex flex-col justify-start py-2 px-2'>{children}</div>
       <Footer />
     </div>
   )
